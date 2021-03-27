ef520f4a844f781e0b16460012a620064f3e7866
Yago Lopes <yagolopeslazaro@gmail.com>
2021-03-27 11:43:18 -0300

    add anotations

--- src/App.js
+++ src/App.js
@@ -1,94 +1,15 @@
-import React, { useRef } from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as facemesh from "@tensorflow-models/facemesh";
-import Webcam from "react-webcam";
-import "./App.css";
-import { drawMesh } from "./utilities";
+import * as p5 from "p5";
+import "p5/lib/addons/p5.dom";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as faceapi from "face-api.js";
 
-function App() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
+const MODEL_URL = "/models";
 
-  // Load facemesh
-  const runFacemesh = async () => {
-    const net = await facemesh.load({
-      inputResolution: { width: 640, height: 480 },
-      scale: 0.8,
-    });
-    setInterval(() => {
-      detect(net);
-    }, 100);
-  };
+export default function sketch(p) {
+  let capture = null;
 
-  // Detect function
-  const detect = async (net) => {
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      // Get Video Properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+  let cocossdModel = null;
 
-      // Set Video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
-
-      // Set Canvas width
-
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
-
-      // Make detections
-
-      const face = await net.estimateFaces(video);
-      // console.log(face);
-
-      // Get canvas context for drawing
-
-      const ctx = canvasRef.current.getContext("2d");
-      drawMesh(face, ctx);
-    }
-  };
-
-  runFacemesh();
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zIndex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zIndex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-      </header>
-    </div>
-  );
+  let cocoDrawings = [];
+  let faceDrawings = [];
 }
-
-export default App;
