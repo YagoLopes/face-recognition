aa2dd5d84fd9bd4c635c119d8ceabf7b6770cd1e
Yago Lopes <yagolopeslazaro@gmail.com>
2021-03-27 11:13:19 -0300

    add modelo classificador

--- src/App.js
+++ src/App.js
@@ -1,94 +1,172 @@
-import React, { useRef } from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as facemesh from "@tensorflow-models/facemesh";
-import Webcam from "react-webcam";
-import "./App.css";
-import { drawMesh } from "./utilities";
+import React, { useRef, useEffect, useState } from "react";
 
-function App() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs";
 
-  // Load facemesh
-  const runFacemesh = async () => {
-    const net = await facemesh.load({
-      inputResolution: { width: 640, height: 480 },
-      scale: 0.8,
-    });
-    setInterval(() => {
-      detect(net);
-    }, 100);
-  };
+const App = () => {
+  const [records, setRecords] = useState([]);
 
-  // Detect function
-  const detect = async (net) => {
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      // Get Video Properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+  const videoElement = useRef(null);
+  const startButtonElement = useRef(null);
+  const stopButtonElement = useRef(null);
 
-      // Set Video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+  const shouldRecordRef = useRef(false);
+  const modelRef = useRef(null);
+  const recordingRef = useRef(false);
+  const lastDetectionsRef = useRef([]);
+  const recorderRef = useRef(null);
 
-      // Set Canvas width
+  useEffect(() => {
+    async function prepare() {
+      startButtonElement.current.setAttribute("disabled", true);
+      stopButtonElement.current.setAttribute("disabled", true);
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+          });
+          window.stream = stream;
+          videoElement.current.srcObject = stream;
+          const model = await cocoSsd.load();
 
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
-
-      // Make detections
-
-      const face = await net.estimateFaces(video);
-      // console.log(face);
-
-      // Get canvas context for drawing
-
-      const ctx = canvasRef.current.getContext("2d");
-      drawMesh(face, ctx);
+          modelRef.current = model;
+          startButtonElement.current.removeAttribute("disabled");
+        } catch (error) {
+          console.error(error);
+        }
+      }
     }
-  };
+    prepare();
+  }, []);
 
-  runFacemesh();
+  async function detectFrame() {
+    if (!shouldRecordRef.current) {
+      stopRecording();
+      return;
+    }
+
+    const predictions = await modelRef.current.detect(videoElement.current);
+
+    let foundPerson = false;
+    for (let i = 0; i < predictions.length; i++) {
+      if (predictions[i].class === "person") {
+        foundPerson = true;
+      }
+    }
+
+    if (foundPerson) {
+      startRecording();
+      lastDetectionsRef.current.push(true);
+    } else if (lastDetectionsRef.current.filter(Boolean).length) {
+      startRecording();
+      lastDetectionsRef.current.push(false);
+    } else {
+      stopRecording();
+    }
+
+    lastDetectionsRef.current = lastDetectionsRef.current.slice(
+      Math.max(lastDetectionsRef.current.length - 10, 0)
+    );
+
+    requestAnimationFrame(() => {
+      detectFrame();
+    });
+  }
+
+  function startRecording() {
+    if (recordingRef.current) {
+      return;
+    }
+
+    recordingRef.current = true;
+    console.log("start recording");
+
+    recorderRef.current = new MediaRecorder(window.stream);
+
+    recorderRef.current.ondataavailable = function (e) {
+      const title = new Date() + "";
+      const href = URL.createObjectURL(e.data);
+      setRecords((previousRecords) => {
+        return [...previousRecords, { href, title }];
+      });
+    };
+
+    recorderRef.current.start();
+  }
+
+  function stopRecording() {
+    if (!recordingRef.current) {
+      return;
+    }
+
+    recordingRef.current = false;
+    recorderRef.current.stop();
+    console.log("stopped recording");
+    lastDetectionsRef.current = [];
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zIndex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zIndex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-      </header>
+    <div className="p-3">
+      <div>
+        <video autoPlay playsInline muted ref={videoElement} />
+      </div>
+      <div>
+        <div className="btn-toolbar" role="toolbar">
+          <div className="btn-group mr-2" role="group">
+            <button
+              className="btn btn-success"
+              onClick={() => {
+                shouldRecordRef.current = true;
+                stopButtonElement.current.removeAttribute("disabled");
+                startButtonElement.current.setAttribute("disabled", true);
+                detectFrame();
+              }}
+              ref={startButtonElement}
+            >
+              Start
+            </button>
+          </div>
+          <div className="btn-group mr-2" role="group">
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                shouldRecordRef.current = false;
+                startButtonElement.current.removeAttribute("disabled");
+                stopButtonElement.current.setAttribute("disabled", true);
+                stopRecording();
+              }}
+              ref={stopButtonElement}
+            >
+              Stop
+            </button>
+          </div>
+        </div>
+        <div className="row p-3">
+          <h3>Records:</h3>
+          {!records.length
+            ? null
+            : records.map((record) => {
+                return (
+                  <div className="card mt-3 w-100" key={record.title}>
+                    <div className="card-body">
+                      <h5 className="card-title">{record.title}</h5>
+                      <video
+                        id="playerVideo"
+                        width="450px"
+                        height="338px"
+                        controls
+                        src={record.href}
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+// ReactDOM.render(<App />, document.getElementById("root"));
