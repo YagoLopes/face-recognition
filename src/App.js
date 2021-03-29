39abacc287c86173e627ec3fd50d37cd658b6b42
Yago Lopes <yagolopeslazaro@gmail.com>
2021-03-29 18:10:24 -0300

    add react + face-api + ssl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,99 @@
-import React from "react";
-import "./App.css";
-import objectDetectionSketch from "./ObjectDetectionSketch";
+import React, { useRef, useEffect } from "react";
+import * as faceapi from "face-api.js";
 
-import P5Wrapper from "react-p5-wrapper";
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-export default class app extends React.Component {
-  render() {
-    return <P5Wrapper sketch={objectDetectionSketch} />;
-  }
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + "/models";
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo);
+    };
+    loadModels();
+  }, []);
+
+  const startVideo = () => {
+    navigator.getUserMedia(
+      {
+        video: {},
+      },
+      (stream) => (webcamRef.current.srcObject = stream),
+      function () {
+        console.warn("Error getting audio stream from getUserMedia");
+      }
+    );
+  };
+
+  const handleVideoPlay = () => {
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(
+          webcamRef.current,
+          new faceapi.TinyFaceDetectorOptions()
+        )
+        //Pede para detectar
+        .withFaceLandmarks()
+        .withFaceExpressions();
+
+      const resizedDetections = faceapi.resizeResults(detections, {
+        width: canvasRef.current.width,
+        height: canvasRef.current.height,
+      });
+
+      // Pede para limpar o canvas
+      canvasRef.current
+        .getContext("2d")
+        .clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+      //Pede para desenhar
+      faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+    }, 1000);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <video
+          ref={webcamRef}
+          autoPlay
+          muted
+          onPlay={handleVideoPlay}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
 }
+
+export default App;
